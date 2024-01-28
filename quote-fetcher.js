139d5cf6b7645875ef3eb9eb4e9dd2d88299438f
Valeri <valeri.torishnya@gmail.com>
2024-01-28 23:38:04 +0200

    update code

--- quote-fetcher.js
+++ quote-fetcher.js
@@ -7,41 +7,42 @@ const template = `
 `
 
 class QuoteFetcher extends HTMLElement {
-    constructor () {
-        super();
-        this.attachShadow({ mode: 'open' });
-        this.shadowRoot.innerHTML = template;
-    }
+  constructor () {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.innerHTML = template;
+  }
 
-    connectedCallback () {
-        this.refresh();
+  connectedCallback () {
+    this.refresh();
 
-        document.body.addEventListener('click', () => {
-            this.refresh();
-        })
-    }
+    document.body.addEventListener('click', () => {
+      this.refresh();
+    })
+  }
 
-    refresh () {
-        fetch('https://animechan.xyz/api/random')
-        .then(res => res.json())
-        .then(json => {
-            Object.assign(this.dataset, json);
-        });
-    }
+  refresh () {
+    fetch('https://api.quotable.io/random')
+      .then(res => res.json())
+      .then(json => {
+          this.setAttribute('data-character', json.author);
+          this.setAttribute('data-quote', json.content);
+      });
+  }
 
-    static get observedAttributes () {
-        return [
-            'data-character',
-            'data-quote'
-        ]
-    }
+  static get observedAttributes () {
+    return [
+      'data-character',
+      'data-quote'
+    ]
+  }
 
-    attributeChangedCallback (attr, prev, value) {
-        this[attr](value, this.shadowRoot.querySelector(`[${attr}]`));
-    }
+  attributeChangedCallback (attr, prev, value) {
+    this[attr](value, this.shadowRoot.querySelector(`[${attr}]`));
+  }
 
-    ['data-character'] (value, el) { el.textContent = value; }
-    ['data-quote'] (value, el) { el.textContent = value; }
+  ['data-character'] (value, el) { el.textContent = value; }
+  ['data-quote'] (value, el) { el.textContent = value; }
 }
 
 customElements.define('quote-fetcher', QuoteFetcher);
